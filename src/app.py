8ec8aed53c16ddd211aa8403ec664fe18f39804a
Daren Hua <59425042+darenhua@users.noreply.github.com>
2024-02-24 14:12:01 -0500

    starting

--- src/app.py
+++ src/app.py
@@ -1,14 +1,8 @@
 from datetime import datetime
 from typing import Tuple
+from dotenv import load_dotenv
+import os
 
-from pydantic import BaseModel
+load_dotenv()
 
-
-class Delivery(BaseModel):
-    timestamp: datetime
-    dimensions: Tuple[int, int]
-
-
-m = Delivery(timestamp="2020-01-02T03:04:05Z", dimensions=["10", "20"])
-print(repr(m.timestamp))
-print(m.dimensions)
+openai_api_key = os.environ["OPENAI_API_KEY"]
